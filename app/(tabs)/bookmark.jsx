6bd5efd3adf82c2f02aa159c31212d59d0634f2d
Art San <artsan.mcc@gmail.com>
2024-04-30 21:54:40 +0700

    main screen

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+const bookmark = () => {
+  return (
+    <View>
+      <Text>bookmark</Text>
+    </View>
+  )
+}
+export default bookmark
+const styles = StyleSheet.create({})
