f0f5b8b7393b54b243028d2ac3b7acc4dd82c928
Art San <artsan.mcc@gmail.com>
2024-05-01 16:55:11 +0700

    ok

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -36,13 +36,13 @@ const RooyLayout = () => {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen
-        name="search"
+        name="index"
         options={{ headerShown: false }} // убирает заголовок от путей
       />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="search" options={{ headerShown: false }} />
     </Stack>
   )
 }
