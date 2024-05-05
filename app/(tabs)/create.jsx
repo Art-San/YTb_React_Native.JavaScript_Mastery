e69943b2bb16b042e027889068ecc746689f0708
Art San <artsan.mcc@gmail.com>
2024-05-05 16:04:53 +0700

    ok

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,14 +1,23 @@
-import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native'
 // 3:42:00
 {
   /* 3:50:00 */
 }
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { FormField } from '../../components'
+import { CustomButton, FormField } from '../../components'
 import { useGlobalContext } from '../../context/GlobalProvider'
 import { useState } from 'react'
 import { ResizeMode, Video } from 'expo-av'
 import { icons } from '../../constants'
+import { createVideoPost } from '../../lib/appwrite'
+import * as DocumentPicker from 'expo-document-picker'
 const Create = () => {
   const { user } = useGlobalContext()
   const [uploading, setUploading] = useState(false)
@@ -18,6 +27,69 @@ const Create = () => {
     thumbnail: null,
     prompt: ''
   })
+
+  const openPicker = async (selectType) => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type:
+        selectType === 'image'
+          ? ['image/png', 'image/jpg']
+          : ['video/mp4', 'video/gif']
+    })
+
+    if (!result.canceled) {
+      if (selectType === 'image') {
+        setForm({
+          ...form,
+          thumbnail: result.assets[0]
+        })
+      }
+
+      if (selectType === 'video') {
+        setForm({
+          ...form,
+          video: result.assets[0]
+        })
+      }
+    } else {
+      setTimeout(() => {
+        Alert.alert('Document picked', JSON.stringify(result, null, 2))
+      }, 100)
+    }
+  }
+
+  const submit = async () => {
+    if (
+      (form.prompt === '') |
+      (form.title === '') |
+      !form.thumbnail |
+      !form.video
+    ) {
+      return Alert.alert('Пожалуйста, заполните все поля')
+    }
+
+    setUploading(true)
+    try {
+      await createVideoPost({
+        ...form,
+        userId: user.$id
+      })
+
+      Alert.alert('Success', 'Пост успешно загружен')
+      router.push('/home')
+    } catch (error) {
+      console.log(0, 'Error Create-screen', error.message)
+      Alert.alert('Error', error.message)
+    } finally {
+      setForm({
+        title: '',
+        video: null,
+        thumbnail: null,
+        prompt: ''
+      })
+
+      setUploading(false)
+    }
+  }
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView className="px-4 my-6">
@@ -38,7 +110,7 @@ const Create = () => {
             Upload Video
           </Text>
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => openPicker('video')}>
             {form.video ? (
               <Video
                 source={{ uri: form.video.uri }}
@@ -65,8 +137,42 @@ const Create = () => {
           <Text className="text-base text-gray-100 font-pmedium">
             Миниатюра
           </Text>
-          {/* 3:50:00 */}
+          <TouchableOpacity onPress={() => openPicker('image')}>
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode="cover"
+                className="w-full h-64 rounded-2xl"
+              />
+            ) : (
+              <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 flex justify-center items-center flex-row space-x-2">
+                <Image
+                  source={icons.upload}
+                  resizeMode="contain"
+                  alt="upload"
+                  className="w-5 h-5"
+                />
+                <Text className="text-sm text-gray-100 font-pmedium">
+                  Выберите файл
+                </Text>
+              </View>
+            )}
+          </TouchableOpacity>
         </View>
+        <FormField
+          title="AI Prompt"
+          value={form.prompt}
+          placeholder="Подсказка AI вашего видео..."
+          handleChangeText={(e) => setForm({ ...form, prompt: e })}
+          otherStyles="mt-7"
+        />
+
+        <CustomButton
+          title="Отправить и опубликовать"
+          handlePress={submit}
+          containerStyles="mt-7"
+          isLoading={uploading}
+        />
       </ScrollView>
     </SafeAreaView>
   )
